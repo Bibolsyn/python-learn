1e4f067b52494d64fb0b191638ad8dd7e35f61e9
Bibolsyn <bezdejb@gmail.com>
2020-07-07 17:20:55 +0600

    Compilation error

--- dsa.py
+++ dsa.py
@@ -1,9 +1,5 @@
-Bibolsyn = input()
-x = len(Bibolsyn)
-i = 0
-x = x - 1
-k = 0
-if k == 1:
-  print("NO")
-else:
-  print("YES")
+x,x1,x2,x3,x4=input()
+if(x1=x4 or x2=x3): 
+	print("YES") 
+else: 
+	print("NO")
