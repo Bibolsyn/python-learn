f9914bf986b47604aedf44b879403b49b5285dc0
Bibolsyn Baigaraev <67378904+Bibolsyn@users.noreply.github.com>
2020-07-17 14:01:21 +0600

    Add files via upload

--- Acmp/Hellow.py
+++ Acmp/Hellow.py
@@ -1,4 +1,8 @@
-k=int(input())
-for i in range(k):
-	n,m= map(int,input().strip().split())
-	print(int(19*m + (n + 239)*(n + 366) / 2))
+c=input() # t
+l=["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m","q"]
+for i in range(len(l)):
+	if(l[i]==c): 
+		print(l[i+1])
+		break
+	
+ 
